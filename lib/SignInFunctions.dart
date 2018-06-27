53598adbc2e1c11060fa7181161f9ecfe2fc9dc4
uncoded-decimal <30970288+uncoded-decimal@users.noreply.github.com>
2018-06-27 14:27:44 +0530

    First Commit

---
+++ lib/SignInFunctions.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+//void main() {  runApp(new MyApp());}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Firebase Auth Demo',
+      home: new MyHomePage(title: 'Firebase Auth Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _message = new Future<String>.value('');
+
+  Future<String> _testSignInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+    await googleUser.authentication;
+    final FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signInWithGoogle succeeded: $user';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new MaterialButton(
+              child: const Text('Test signInAnonymously'),
+              onPressed: signOut),
+          new MaterialButton(
+            color: Colors.white,
+              splashColor: Colors.red,
+              child: const Text('Test signInWithGoogle'),
+              onPressed: () {
+                setState(() {
+                  _message = _testSignInWithGoogle();
+                });
+              }),
+          new FutureBuilder<String>(
+              future: _message,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                return new Text(snapshot.data ?? '',
+                    style: const TextStyle(
+                        color: const Color.fromARGB(255, 0, 155, 0)));
+              }),
+        ],
+      ),
+    );
+  }
+
+  void signOut() async {
+      await _googleSignIn.signOut();
+      await FirebaseAuth.instance.signOut();
+      if(_auth.currentUser() != null)
+        print('Fucked Outta here!: ');
+      if(_googleSignIn.currentUser == null)
+        print('Fucked outta everywhere');
+      if(_auth.currentUser() == Future.value(null))
+        print('OAOAOA');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new TabLayoutDemo());
+
+class TabLayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      color: Colors.yellow,
+      home: DefaultTabController(
+        length: 4,
+        child: new Scaffold(
+          body: TabBarView(
+            children: [
+              new Container(
+                color: Colors.yellow,
+              ),
+              new Container(color: Colors.orange,),
+              new Container(
+                color: Colors.lightGreen,
+              ),
+              new Container(
+                color: Colors.red,
+              ),
+            ],
+          ),
+          bottomNavigationBar: new TabBar(
+            tabs: [
+              Tab(
+                icon: new Icon(Icons.home),
+              ),
+              Tab(
+                icon: new Icon(Icons.rss_feed),
+              ),
+              Tab(
+                icon: new Icon(Icons.perm_identity),
+              ),
+              Tab(icon: new Icon(Icons.settings),)
+            ],
+            labelColor: Colors.yellow,
+            unselectedLabelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.red,
+          ),
+          backgroundColor: Colors.black,
+        ),
+      ),
+    );
+  }
+}
