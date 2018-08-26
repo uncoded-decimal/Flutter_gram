8589607cf0702be9ceacadf9ea67ec1b83ee06a9
uncoded_decimal <adityanigam13@gmail.com>
2018-08-25 18:02:03 -0700

    Log In screen complete!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new TabLayoutDemo());
-
 class TabLayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
